94779d794b54ebc4686d626f47570881342a0efb
DEBORAH HONG <deborahhong@DEBORAHs-MacBook-Air.local>
2020-11-15 16:00:59 -0500

    update router.js

--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const router = express.Router();
+const ROUTES_FOLDER = "/routes";
 
 const buildRoutes = (directoryPath, route) => {
-  const rmRoutesFolder = route.replace('/routes', '')
-  const setRoute = async (rStr) => {
-    console.log('URL: ', rmRoutesFolder, rStr);
+  const rmRoutesFolder = route.replace(ROUTES_FOLDER, "");
+  const setRoute = async (rStr) =>
     await router.use(
-      `${rmRoutesFolder}/${rStr === "index" ? "" : rStr}`, require(`.${route}/${rStr}`)
+      `${rmRoutesFolder}/${rStr === "index" ? "" : rStr}`,
+      require(`.${route}/${rStr}`)
     );
-  }
-  console.log(directoryPath);
+
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
       return console.log("Unable to scan directory: " + err);
@@ -19,14 +19,16 @@ const buildRoutes = (directoryPath, route) => {
 
     files.forEach((file) => {
       if (file.endsWith(".js")) {
-        // console.log("FILE: ", file);
         setRoute(file.replace(".js", ""));
       } else {
-        buildRoutes(path.join(__dirname, `${route}/${file}`), `${route}/${file}`);
+        buildRoutes(
+          path.join(__dirname, `${route}/${file}`),
+          `${route}/${file}`
+        );
       }
     });
   });
 };
-buildRoutes(path.join(__dirname, '/routes'), '/routes');
+buildRoutes(path.join(__dirname, ROUTES_FOLDER), ROUTES_FOLDER);
 
 module.exports = router;
